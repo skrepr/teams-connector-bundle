c8cc8564ee7eac8999c96ae611d3dc861075329b
Michael Erkens <michael@skrepr.com>
2023-01-09 10:12:17 +0100

    Bump skrper/teams-connector + Added tests for Symfony 6.2 and PHP 8.2

--- phpunit-in-docker.sh
+++ phpunit-in-docker.sh
@@ -11,7 +11,7 @@ then
 fi
 
 # Define supported PHP versions
-for phpversion in 7.4 8.0 8.1
+for phpversion in 7.4 8.0 8.1 8.2
 do
   dockerimagename=${PROJECT_NAME}-php-dev:${phpversion}
   echo "Builder docker image for PHP ${phpversion}... (${dockerimagename})"
@@ -19,7 +19,7 @@ do
   echo "Starting container..."
   docker run -d --name ${PROJECT_NAME} --volume $(pwd):/data:rw ${dockerimagename} tail -f /dev/null > /dev/null
   echo "Installing composer packages..."
-  docker exec ${PROJECT_NAME} composer install -q -n
+  docker exec ${PROJECT_NAME} composer install --quiet --no-interaction --no-plugins
   echo "Running PHPUnit..."
   docker exec ${PROJECT_NAME} vendor/bin/phpunit
   echo "Clean up..."
@@ -28,14 +28,15 @@ do
 done
 
 # Next, test to see if it can be installed in Symfony
-PHP_VERSION_FOR_SYMFONY_TESTS=8.1
+PHP_VERSION_FOR_SYMFONY_TESTS=8.2
 
 dockerimagename=${PROJECT_NAME}-php-dev:${PHP_VERSION_FOR_SYMFONY_TESTS}
 docker run -d --name ${PROJECT_NAME} --volume $(pwd):/data:rw ${dockerimagename} tail -f /dev/null
-for symfonyversion in 4.4 5.4 6.0
+for symfonyversion in 4.4 5.4 6.0 6.1 6.2
 do
   echo "Create Symfony ${symfonyversion} skeleton..."
-  docker exec ${PROJECT_NAME} composer create-project -q -n symfony/skeleton:^${symfonyversion} /tmp/test-${symfonyversion}
+  docker exec ${PROJECT_NAME} composer create-project -q -n symfony/skeleton:${symfonyversion}.* /tmp/test-${symfonyversion}
+  docker exec -w /tmp/test-${symfonyversion} ${PROJECT_NAME} composer config --no-plugins allow-plugins true
   echo "skrepr_teams_connector:" | docker exec -i ${PROJECT_NAME} tee /tmp/test-${symfonyversion}/config/packages/teams.yaml > /dev/null
   echo "  endpoint: http://localhost/test" | docker exec -i ${PROJECT_NAME} tee -a /tmp/test-${symfonyversion}/config/packages/teams.yaml > /dev/null
   docker exec -w /tmp/test-${symfonyversion} ${PROJECT_NAME} composer config repositories.teams-connector-bundle path /data
